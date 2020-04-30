f47bfcef3131e853b96c53443c235dbea9de6b29
darshanjag <itsdarshanjagtap@gmail.com>
2020-04-30 17:10:15 +0530

    instance method for verifying password

--- DarshanJagtap_BackendInternship_Assignment/models/userModel.js
+++ DarshanJagtap_BackendInternship_Assignment/models/userModel.js
@@ -3,42 +3,47 @@ const validator = require('validator');
 const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    lowercase: true,
-    validate: [validator.isEmail, 'please enter a valid email'],
-  },
-  password: {
-    type: String,
-    required: [true, 'please enter a password'],
-    minlength: 8,
-  },
-  passwordConfirm: {
-    type: String,
-    required: [true, 'please confirm your password'],
-    validate: {
-      validator: function (el) {
-        return el === this.password;
-      },
-      message: 'passwords did not match',
+    name: {
+        type: String,
+        required: true,
     },
-  },
-  avatar: {
-    type: String,
-  },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        validate: [validator.isEmail, 'please enter a valid email'],
+    },
+    password: {
+        type: String,
+        required: [true, 'please enter a password'],
+        minlength: 8,
+        select: false
+    },
+    passwordConfirm: {
+        type: String,
+        required: [true, 'please confirm your password'],
+        validate: {
+            validator: function (el) {
+                return el === this.password;
+            },
+            message: 'passwords did not match',
+        },
+    },
+    avatar: {
+        type: String,
+    },
 });
 
 userSchema.pre('save', async function (next) {
-  if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  this.passwordConfirm = undefined;
+    if (!this.isModified('password')) return next();
+    this.password = await bcrypt.hash(this.password, 12);
+    this.passwordConfirm = undefined;
 });
+
+userSchema.methods.correctPassword = async function (candidatePassword, userPassword) {
+    return await bcrypt.compare(candidatePassword, userPassword);
+}
 
 const User = mongoose.model('USER', userSchema);
 module.exports = User;
