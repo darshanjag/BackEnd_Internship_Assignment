db68f195409c2abc41057471b1817296eea5f58e
darshanjag <itsdarshanjagtap@gmail.com>
2020-04-30 15:17:16 +0530

    signup fix

--- DarshanJagtap_BackendInternship_Assignment/controllers/authController.js
+++ DarshanJagtap_BackendInternship_Assignment/controllers/authController.js
@@ -1,17 +1,20 @@
 const User = require('./../models/userModel');
 
 exports.signup = async (req, res, next) => {
-    try {
-        const newUser = await User.create(req.body);
-        console.log(req.body)
-        res.status(201).json({
-            status: 'succecss',
-            data: {
-                user: newUser
-            }
-        })
-    } catch (err) {
-        console.log(err)
-    }
-
-}
+  try {
+    const newUser = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.passwored,
+      passwordConfirm: req.body.passwordConfirm,
+    });
+    res.status(201).json({
+      status: 'succecss',
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
