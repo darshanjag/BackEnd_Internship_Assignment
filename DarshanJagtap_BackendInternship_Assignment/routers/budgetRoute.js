c594e30f019dc3bd66fd98afdd1e73fb2b4a51a6
darshanjag <itsdarshanjagtap@gmail.com>
2020-05-01 13:18:54 +0530

    budget route correction

--- DarshanJagtap_BackendInternship_Assignment/routers/budgetRoute.js
+++ DarshanJagtap_BackendInternship_Assignment/routers/budgetRoute.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const budgetController = require('./../controllers/budgetController');
+const authController = require('./../controllers/authController');
+
 const router = mongoose.Route();
 
 router.route('/budget')
-.post(budgetController.addBudget);
+.post(authController.protect, budgetController.addBudget);
