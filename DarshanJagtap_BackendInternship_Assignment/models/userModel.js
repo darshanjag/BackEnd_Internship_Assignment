c0301de5590119d3ed84fc0fc7c3600d53ddb874
darshanjag <itsdarshanjagtap@gmail.com>
2020-05-04 22:44:00 +0530

    validation removed

--- DarshanJagtap_BackendInternship_Assignment/models/userModel.js
+++ DarshanJagtap_BackendInternship_Assignment/models/userModel.js
@@ -16,7 +16,6 @@ const userSchema = mongoose.Schema({
     password: {
         type: String,
         required: [true, 'please enter a password'],
-        minlength: 8,
         select: false
     },
     passwordConfirm: {
